d06d98899238f6232d77fed4771614ceabbc0653
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-06-27 11:02:42 +1200

    Add files via upload

--- 01_math_base_component_v2.py
+++ 01_math_base_component_v2.py
@@ -67,6 +67,49 @@ show_instructions = yes_no("would you like to see the instructions?")
 if show_instructions == "yes":
     instructions()
 
+# Number checking function goes here
+def intcheck(question, low=None, high=None, exit_code = None):
+
+    while True:
+
+        # sets up error messages
+        if low is not None and high is not None:
+            error = "Please enter an integer between {} and {} (inclusive)".format(low, high)
+        elif low is not None and high is None:
+            error = "Please enter an integer that is more than or equal to {}".format(low)
+        elif low is None and high is not None:
+            error = "Please enter an integer that is less than or equal to {}".format(high)
+        else:
+            error = "Please enter an integer"
+
+        try:
+            response = input(question)
+            
+            # check to see if response is the exit code and return it
+            if response == exit_code:
+                return response
+            
+            # change the response into an integer
+            else:
+                response = int(response)
+
+            # Checks response is not too low, not use of 'is not' keywords
+            if low is not None and response < low:
+                print(error)
+                continue
+
+            # Checks response is not too high
+            if high is not None and response > high:
+                print(error)
+                continue
+
+            return response
+
+        # checks input is a integer
+        except ValueError:
+            print(error)
+            continue
+
 choose_difficulty = ("Press <E> for Easy, <M> for Medium or <H> for Hard")
 easy_mode = 0
 medium_mode = 0
@@ -84,6 +127,7 @@ print()
 print("You chose {}".format(choose))
 print()
 
+# asks user what difficulty they would like to play on
 if choose == "E":
     print("you chose to play on easy difficulty")
 
@@ -97,14 +141,12 @@ print()
 print("Question 1:")
 print()
 
-
-if choose_difficulty == "E":
-
-    end = False
+end = False
 questions_asked = 0
 correct_questions = 0
 incorrect_questions = 0
 
+# generates a random number and question for user to attempt
 while end == False:
     if questions_asked <= 10 | questions_asked >=10:
         choice = random.choice("+-")
@@ -113,15 +155,48 @@ while end == False:
         print(number_one, choice, number_two)
         answer = int(input("please answer the question"))
 
+# checks if user got the addition question correct or not
     if choice == "+":
         actual_answer = number_one + number_two
-        print ("congrats, you got it")
+        if answer == actual_answer:
+            print ("congrats, you got it")
+            correct_answers = 1
+
+        else:
+            print ("Sorry you got it wrong")
     
+# checks if user got the subtraction question correct or not    
     elif choice == "-":
         actual_answer = number_one - number_two
         if answer == actual_answer:
             print ("congrats, you got it")
+            correct_answers = 1
 
-    else: 
-        print ("sorry, you got it wrong")
-        break
+        else: 
+            print ("sorry, you got it wrong")
+
+
+end = False
+questions_asked = 0
+correct_questions = 0
+incorrect_questions = 0
+
+# generates a random number and question for user to attempt
+while end == False:
+    if questions_asked <= 10 | questions_asked >=10:
+        choice = random.choice("*")
+        number_one = random.randint(1,10)
+        number_two = random.randint(1,10)
+        print(number_one, choice, number_two)
+        answer = int(input("please answer the question"))
+
+# checks if user got the addition question correct or not
+    if choice == "*":
+        actual_answer = number_one * number_two
+        if answer == actual_answer:
+            print ("congrats, you got it")
+            correct_answers = 1
+
+        else:
+            print ("Sorry you got it wrong")
+    
--- 01_medium_questions.py
+++ 01_medium_questions.py
@@ -1,22 +1,23 @@
-import random 
+import random
 import math
-
 
 end = False
 questions_asked = 0
 correct_questions = 0
 incorrect_questions = 0
 
+# generates a random number and question for user to attempt
 while end == False:
     if questions_asked <= 10 | questions_asked >=10:
-        choice = random.choice("+-")
-        number_one = random.randint(1,20)
-        number_two = random.randint(1,20)
+        choice = random.choice("*")
+        number_one = random.randint(1,10)
+        number_two = random.randint(1,10)
         print(number_one, choice, number_two)
         answer = int(input("please answer the question"))
 
-    if choice == "+":
-        actual_answer = number_one + number_two
+# checks if user got the addition question correct or not
+    if choice == "*":
+        actual_answer = number_one * number_two
         if answer == actual_answer:
             print ("congrats, you got it")
             correct_answers = 1
@@ -24,11 +25,3 @@ while end == False:
         else:
             print ("Sorry you got it wrong")
     
-    elif choice == "-":
-        actual_answer = number_one - number_two
-        if answer == actual_answer:
-            print ("congrats, you got it")
-            correct_answers = 1
-
-        else: 
-            print ("sorry, you got it wrong")
